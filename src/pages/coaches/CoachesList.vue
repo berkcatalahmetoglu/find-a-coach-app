3e0eb7ac6fd2a86908bdf017e40e9d9265279ec0
Berk Catalahmetoglu <berk.catalahmetoglu@beymen.com>
2023-02-27 11:12:38 +0300

    Coaches List is created with props

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,14 +6,16 @@
             <router-link to="/register">Register as Coach</router-link>
         </div>
         <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}</li>
+            <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
+                :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
         </ul>
         <h3 v-else>No coaches found.</h3>
     </section>
 </template>
 
 <script>
+
+import CoachItem from '../../components/coaches/CoachItem.vue'
 
 export default {
     computed: {
@@ -23,6 +25,9 @@ export default {
         hasCoaches() {
             return this.$store.getters['coaches/hasCoaches']
         }
+    },
+    components: {
+        CoachItem
     }
 }
 </script>
