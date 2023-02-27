f5bd37a2813c1622dcf25892fdd6f772ed47ece0
Berk Catalahmetoglu <berk.catalahmetoglu@beymen.com>
2023-02-27 14:45:34 +0300

    Added feature of filtering coaches

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,100 @@
+<template>
+    <base-card>
+        <h2>
+            Find Your Coach
+        </h2>
+        <span class="filter-option">
+            <input type="checkbox" id="frontend" checked @change="setFilter" />
+            <label for="frontend">Frontend</label>
+        </span>
+        <span class="filter-option">
+            <input type="checkbox" id="backend" checked @change="setFilter" />
+            <label for="Backend">Backend</label>
+        </span>
+        <span class="filter-option">
+            <input type="checkbox" id="career" checked @change="setFilter" />
+            <label for="Career">Career</label>
+        </span>
+    </base-card>
+</template>
+
+<script>
+export default {
+    emits: ['change-filter'],
+    data() {
+        return {
+            filters: {
+                frontend: true,
+                backend: true,
+                career: true
+            }
+        }
+    },
+    methods: {
+        setFilter(event) {
+            const inputId = event.target.id;
+            const isActive = event.target.checked;
+
+            const updatedFilters = {
+                ...this.filters,
+                [inputId]: isActive
+            }
+            this.filters = updatedFilters;
+            this.$emit("change-filter", updatedFilters);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+h2 {
+    margin: 0.5rem 0;
+}
+
+.filter-option {
+    margin: 5px
+}
+
+.center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+input[type="checkbox"] {
+    position: relative;
+    width: 40px;
+    height: 20px;
+    -webkit-appearance: none;
+    background: #c6c6c6;
+    outline: none;
+    top: 7px;
+    border-radius: 20px;
+    box-shadow: inset 0 0 5px rgba(255, 0, 0, 0.2);
+    transition: 0.7s;
+}
+
+input:checked[type="checkbox"] {
+    background: #03a9f4;
+}
+
+input[type="checkbox"]:before {
+    content: '';
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border-radius: 20px;
+    top: 0;
+    left: 0;
+    background: #ffffff;
+    transform: scale(1.1);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    transition: .5s;
+}
+
+input:checked[type="checkbox"]:before {
+    left: 20px;
+}
+</style>
