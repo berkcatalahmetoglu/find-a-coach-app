5a417168d0c8c421dce8b4cbc723f3a331f7bb44
berkcatalahmetoglu <berkkcatall@gmail.com>
2023-03-05 17:54:08 +0300

    added not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-
-    </div>
+    <section>
+        <base-card>
+            <h2>Page Not Found</h2>
+            <p>This page could not be found - maybe check put all our <router-link to="/coaches">coaches.</router-link></p>
+        </base-card>
+    </section>
 </template>
